be88962f4e3292945e2e19e5b5f06215aa93ea8c
joshtestBlueBox <Josh.frazier@bluebox.com>
2015-04-12 21:10:30 -0700

    This is a new file

---
+++ GitTest.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+let myAge = 28
+if myAge >= 27 {
+    println("My name is Josh")
+    
+}else {
+    println("My name is not Josh")
+}
+
+var B = "it"
+
+let it = B
